85328e56e07fb38607ae42b6e9e6c4da16445e4e
Likitha <likithakrishnappaa@gmail.com>
2026-01-05 21:54:54 +0530

    Added  page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Components/Home/Home";
 import ButtonDemo from "./Components/Button/ButtonDemo";
 import InputView from "./Components/Input/InputView";
-import ToggleView from "./Components/Toggle/ToggleView";
+import ToggleView from "./Components/Input/InputView";
 import LoaderPage from "./Components/Loader/LoaderPage";
 import CardPage from "./Components/Card/CardPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="button" element={<ButtonDemo />} />
-          <Route path="input" element={<InputView />} />
-          <Route path="toggle" element={<ToggleView />} />
-          <Route path="loader" element={<LoaderPage />} />
-          <Route path="card" element={<CardPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="button" element={<ButtonDemo />} />
+        <Route path="input" element={<InputView />} />
+        <Route path="toggle" element={<ToggleView />} />
+        <Route path="loader" element={<LoaderPage />} />
+        <Route path="card" element={<CardPage />} />
+      </Route>
+    </Routes>
   );
 }
 
